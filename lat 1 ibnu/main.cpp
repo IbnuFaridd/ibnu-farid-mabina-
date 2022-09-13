4cff70a5db9f4895d673bfb9ab43d36672ef6271
IbnuFaridd <113011904+IbnuFaridd@users.noreply.github.com>
2022-09-13 19:49:52 +0700

    Add files via upload

---
+++ lat 1 ibnu/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama : Ibnu Farid Mabina !" << endl;
+    cout << "NIM : A11.2022.14591!" << endl;
+    cout << "Asal : Kabupaten Tegal !" << endl;
+    cout << "Hobi : Renang !" << endl;
+    return 0;
+}
